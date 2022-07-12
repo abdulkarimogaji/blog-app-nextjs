e09994da8e50a27f8ee24d684c88cf956a24d279
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-07-12 07:21:48 +0100

    feat: provide user context as gloal state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import NavBar from '../components/NavBar'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { UserContextProvider } from '../context/useUserContext';
+
+
+const client = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-  <NavBar />
-  <Component {...pageProps} />
-  </>
+    <QueryClientProvider client={client}>
+      <UserContextProvider>
+        <NavBar />
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </UserContextProvider >
+    </QueryClientProvider>
   )
 }
 
