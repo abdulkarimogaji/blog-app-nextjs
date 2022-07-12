943a6a5b482161dc6c323dabbcbb6e4e7e573fa4
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-07-12 07:24:22 +0100

    feat:add axios interceptor to react-query

---
+++ utils/axios-utils.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const client = axios.create( { baseURL: 'https://blognado.herokuapp.com' })
+
+export const request = ({...options}) => {
+  const token = localStorage.getItem("blognado-access-token")
+  client.defaults.headers.common.Authorization = `Bearer ${token}`
+  const onSuccess = (resp: any) => resp
+  const onError = (err: any) => {
+    console.log(err)
+  }
+  return client(options).then(onSuccess)
+}
