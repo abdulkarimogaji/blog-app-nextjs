c16b9addce2976faeb3ab041ebe4dfb9f07699d6
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-07-17 20:32:35 +0100

    fix: add filter for search tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home = () => {
       <div className="md:w-2/3 w-4/5 flex flex-col justify-center navbar-offset my-center md:pt-16 pt-32  pb-16 gap-4">
         {
           filterTag &&
-        <h1 className='text-xl font-semibold'>Blogs for tag "{filterTag}": <button className='md:p-2 p-1 md:px-4 mr-4 px-2 text-xs md:text-sm action-btn rounded-lg' onClick={() => setFilterTag("")}>Clear</button></h1>
+        <h1 className='text-xl font-semibold'>Blogs for tag &quot{filterTag}&quot: <button className='md:p-2 p-1 md:px-4 mr-4 px-2 text-xs md:text-sm action-btn rounded-lg' onClick={() => setFilterTag("")}>Clear</button></h1>
         }
         {
           data?.data.data.filter(isTag).map(blog => <BlogCard data={blog} key={blog._id} setFilterTag={setFilterTag} />)
