e6231e58575c3126c1b4eb3bd4d620d382e2edf4
Abdulkarim Ogaji <abdulkarimogaji001@gmail.com>
2022-07-22 21:53:34 +0100

    chore: remove client id log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID);
   return (
     <GoogleOAuthProvider
       clientId={process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID!}
