c5c43fe3f266a7169003141ffeed8d4df0cfefe7
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-07-22 17:16:26 +0100

    feat: implement google oauth2 signup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,23 @@ import NavBar from "../components/NavBar";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { UserContextProvider } from "../context/useUserContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={client}>
-      <UserContextProvider>
-        <NavBar />
-        <Component {...pageProps} />
-        <ReactQueryDevtools />
-      </UserContextProvider>
-    </QueryClientProvider>
+    <GoogleOAuthProvider
+      clientId={process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID!}
+    >
+      <QueryClientProvider client={client}>
+        <UserContextProvider>
+          <NavBar />
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </UserContextProvider>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   );
 }
 
--- utils/axios-utils.ts
+++ utils/axios-utils.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const client = axios.create( { baseURL: 'https://blognado.herokuapp.com' })
+const client = axios.create({ baseURL: "https://blognado.herokuapp.com" });
 
-export const request = ({...options}) => {
-  const token = localStorage.getItem("blognado-access-token")
-  client.defaults.headers.common.Authorization = `Bearer ${token}`
-  const onSuccess = (resp: any) => resp
-  const onError = (err: any) => {
-  }
-  return client(options).then(onSuccess)
-}
+export const request = ({ ...options }) => {
+  const token = localStorage.getItem("blognado-access-token");
+  client.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const onSuccess = (resp: any) => resp;
+  const onError = (err: any) => {};
+  return client(options).then(onSuccess);
+};
